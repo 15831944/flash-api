fe001b998704f9ed8f49fb70a6de4e83df2ae45b
rack ratio <mnlife@foxmail.com>
2018-04-16 23:51:47 +0800

    first

--- flash_api/flash_apiDlg.cpp
+++ flash_api/flash_apiDlg.cpp
@@ -7,6 +7,10 @@
 #include "flash_apiDlg.h"
 #include "afxdialogex.h"
 #include "ControlCAN.h"
+#include "open_device.h"
+#include "Login.h"
+#include "CanAnalyse.h"
+//#pragma comment(lib, "ControlCAN.lib")
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
@@ -57,7 +61,10 @@ Cflash_apiDlg::Cflash_apiDlg(CWnd* pParent /*=NULL*/)
 
 void Cflash_apiDlg::DoDataExchange(CDataExchange* pDX)
 {
-	CDialogEx::DoDataExchange(pDX);
+
+
+	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_PROGRESS1, FlashUpdateProgress);
 }
 
 BEGIN_MESSAGE_MAP(Cflash_apiDlg, CDialogEx)
@@ -69,6 +76,20 @@ BEGIN_MESSAGE_MAP(Cflash_apiDlg, CDialogEx)
 	ON_EN_CHANGE(IDC_LEFT, &Cflash_apiDlg::OnEnChangeLeft)
 	ON_EN_CHANGE(IDC_RESULT, &Cflash_apiDlg::OnEnChangeResult)
 	ON_BN_CLICKED(ID_START, &Cflash_apiDlg::OnBnClickedStart)
+	ON_BN_CLICKED(IDC_CLOSE, &Cflash_apiDlg::OnBnClickedClose)
+	ON_BN_CLICKED(IDC_ADD, &Cflash_apiDlg::OnBnClickedAdd)
+	ON_BN_CLICKED(ID_OPENDEVICE, &Cflash_apiDlg::OnBnClickedOpendevice)
+	ON_CBN_SELCHANGE(IDC_module_select, &Cflash_apiDlg::OnCbnSelchangemoduleselect)
+	ON_LBN_SELCHANGE(IDC_LIST1, &Cflash_apiDlg::OnLbnSelchangeList1)
+	ON_NOTIFY(NM_CUSTOMDRAW, IDC_PROGRESS1, &Cflash_apiDlg::OnNMCustomdrawProgress1)
+	ON_CBN_SELCHANGE(IDC_DSP_SELECT, &Cflash_apiDlg::OnCbnSelchangeDspSelect)
+	ON_EN_CHANGE(IDC_MFCEDITBROWSE1, &Cflash_apiDlg::OnEnChangeMfceditbrowse1)
+	ON_STN_CLICKED(IDC_OUT_TXT, &Cflash_apiDlg::OnStnClickedOutTxt)
+	ON_EN_CHANGE(IDC_OUT_TXT, &Cflash_apiDlg::OnEnChangeOutTxt)
+	ON_BN_CLICKED(IDC_BUTTON1, &Cflash_apiDlg::OnBnClickedButton1)
+	ON_EN_CHANGE(IDC_ID, &Cflash_apiDlg::OnEnChangeId)
+	ON_EN_CHANGE(IDC_DLC, &Cflash_apiDlg::OnEnChangeDlc)
+	ON_EN_CHANGE(IDC_DATA, &Cflash_apiDlg::OnEnChangeData)
 END_MESSAGE_MAP()
 
 
@@ -180,7 +201,7 @@ void Cflash_apiDlg::OnBnClickedAdd()
 	SetDlgItemText(IDC_GOOGLE, _T("come on!"));
 	//SetDlgItemText(IDC_RESULT, _T("hello"));/
 	//char hello[] = "hello, world";
-	MessageBox(_T("let's go!"));
+	//MessageBox(_T("let's go!"));
 	//while (1);
 }
 
@@ -219,31 +240,259 @@ void Cflash_apiDlg::OnEnChangeGoogle()
 	// TODO:  在此添加控件通知处理程序代码
 }
 
+/****************************************************************************
+pInitConfig->AccCode AccCode 对应SJA1000 中的四个寄存器ACR0，ACR1，ACR2，
+ACR3，其中高字节对应ACR0，低字节对应ACR3；AccMask
+对应SJA1000 中的四个寄存器AMR0，AMR1，AMR2，AMR3，
+其中高字节对应AMR0，低字节对应AMR3。（请看表后说明）
+pInitConfig->AccMask
+pInitConfig->Reserve
+d
+保留
+pInitConfig->Filter 滤波方式，1 表示单滤波，0 表示双滤波
+pInitConfig->Timing0 波特率定时器0，详见VCI_INIT_CONFIG
+pInitConfig->Timing1 波特率定时器1，详见VCI_INIT_CONFIG
+pInitConfig->Mode 模式，0 表示正常模式，1 表示只听模式
 
+CAN		波特率		定时器0		定时器1
+5Kbps		0xBF		0xFF
+10Kbps		0x31		0x1C
+20Kbps		0x18		0x1C
+40Kbps		0x87		0xFF
+50Kbps		0x09		0x1C
+80Kbps		0x83		0Xff
+100Kbps		0x04		0x1C
+125Kbps		0x03		0x1C
+200Kbps		0x81		0xFA
+250Kbps		0x01		0x1C
+400Kbps		0x80		0xFA
+500Kbps		0x00		0x1C
+666Kbps		0x80		0xB6
+800Kbps		0x00		0x16
+1000Kbps	0x00		0x14
+**************************************************************************/
 void Cflash_apiDlg::OnBnClickedStart()
 {
+
+
+}
+
+
+void Cflash_apiDlg::OnBnClickedClose()
+{
+	FlashUpdateProgress.SetPos(75);
+	// TODO: 在此添加控件通知处理程序代码
+	int device_type = 4;	// CANalyst-II
+	int device_ind = 0;		// first device
+	int can_ind = 0;		// CAN channel 0
+	DWORD	test;
+	test = VCI_CloseDevice(device_type, device_ind);
+	if (test == STATUS_ERR) {
+		MessageBox(_T("关闭设备失败！"), _T("警告"), MB_OK | MB_ICONQUESTION);
+	}
+	else {
+		SetWindowText(_T("Close Device Success!"));
+	}
+	SetDlgItemText(IDC_OUT_TXT, _T("go! go!! go!!!"));
+
+}
+
+
+void Cflash_apiDlg::OnBnClickedButton2()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	// TODO: 在此添加控件通知处理程序代码
+	int left = GetDlgItemInt(IDC_LEFT);
+	int right = GetDlgItemInt(IDC_RIGHT);
+	SetDlgItemInt(IDC_RESULT, left + right);
+	char hello[] = "come on!";
+	SetDlgItemText(IDC_GOOGLE, _T("come on!"));
+	//SetDlgItemText(IDC_RESULT, _T("hello"));/
+	//char hello[] = "hello, world";
+	//MessageBox(_T("let's go!"));
+	//while (1);
+
+	CString	b;
+	b += "\nad离开时间发货不打了卡就卡就是不发链接哈埃里克森的减肥不打了卡时间分布拉斯贷款部分";
+	b += "\nad离开时间发货不打了卡就是不服你就是不发链接哈埃里克森的减肥不打了卡时间分布拉斯贷款部分";
+	SetDlgItemText(IDC_OUT_TXT, b);
+}
+
+
+
+void Cflash_apiDlg::OnBnClickedOpendevice()
+{
+	//Login login_in;
+	//login_in.DoModal();
+	FlashUpdateProgress.SetPos(25);
 	int device_type = 4;	// CANalyst-II
 	int device_ind = 0;		// first device
 	int can_ind = 0;		// CAN channel 0
 	VCI_INIT_CONFIG	vic;
 	DWORD	test;
+	vic.AccCode = 0x00000000;
+	vic.AccMask = 0xFFFFFFFF;
+	vic.Filter = 0;
+	vic.Mode = 0;
+	vic.Timing0 = 0x03;
+	vic.Timing1 = 0x1C;
+	vic.Reserved = 0;
 	test = VCI_OpenDevice(device_type, device_ind, can_ind);
+
 	if (test != STATUS_OK) {
-		MessageBox(_T("Open device failed!!!"), _T("caution"), MB_OK | MB_ICONQUESTION);
+		MessageBox(_T("Open Device Failed!!!"), _T("Caution"), MB_OK | MB_ICONQUESTION);
 		return;
 	}
 
 	test = VCI_InitCAN(device_type, device_ind, can_ind, &vic);
 	if (test == STATUS_ERR) {
 		VCI_CloseDevice(device_type, device_ind);
-		MessageBox(_T("初始化设备失败！"), _T("警告"), MB_OK | MB_ICONQUESTION);
+		MessageBox(_T("Init Device Failed！"), _T("Caution"), MB_OK | MB_ICONQUESTION);
 		return;
 	}
 
 	test = VCI_StartCAN(device_type, device_ind, can_ind);
 	if (test == STATUS_ERR) {
 		VCI_CloseDevice(device_type, device_ind);
-		MessageBox(_T("初始化设备失败！"), _T("警告"), MB_OK | MB_ICONQUESTION);
+		MessageBox(_T("Start Device Failed！"), _T("Caution"), MB_OK | MB_ICONQUESTION);
 		return;
 	}
+	else {
+		SetWindowText(_T("Open Device Success！"));
+	}
+
+}
+
+
+void Cflash_apiDlg::OnCbnSelchangemoduleselect()
+{
+	// TODO: 在此添加控件通知处理程序代码
+}
+
+
+void Cflash_apiDlg::OnLbnSelchangeList1()
+{
+	// TODO: 在此添加控件通知处理程序代码
+}
+
+
+void Cflash_apiDlg::OnNMCustomdrawProgress1(NMHDR *pNMHDR, LRESULT *pResult)
+{
+
+}
+
+
+
+void Cflash_apiDlg::OnCbnSelchangeDspSelect()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	FlashUpdateProgress.SetPos(100);
+}
+
+
+void Cflash_apiDlg::OnEnChangeMfceditbrowse1()
+{
+	// TODO:  如果该控件是 RICHEDIT 控件，它将不
+	// 发送此通知，除非重写 CDialogEx::OnInitDialog()
+	// 函数并调用 CRichEditCtrl().SetEventMask()，
+	// 同时将 ENM_CHANGE 标志“或”运算到掩码中。
+
+	// TODO:  在此添加控件通知处理程序代码
+}
+
+
+void Cflash_apiDlg::OnStnClickedOutTxt()
+{
+	// TODO: 在此添加控件通知处理程序代码
+}
+
+
+void Cflash_apiDlg::OnEnChangeOutTxt()
+{
+	// TODO:  如果该控件是 RICHEDIT 控件，它将不
+	// 发送此通知，除非重写 CDialogEx::OnInitDialog()
+	// 函数并调用 CRichEditCtrl().SetEventMask()，
+	// 同时将 ENM_CHANGE 标志“或”运算到掩码中。
+
+	// TODO:  在此添加控件通知处理程序代码
+}
+
+
+void Cflash_apiDlg::OnBnClickedButton1()
+{
+	OnOK();
+	CanAnalyse	OpenCanDevice;
+	OpenCanDevice.DoModal();
+	/*
+	FlashUpdateProgress.SetPos(50);
+	int device_type = 4;	// CANalyst-II
+	int device_ind = 0;		// first device
+	int can_ind = 0;		// CAN channel 0
+	VCI_INIT_CONFIG	vic;
+	DWORD	test;
+	VCI_CAN_OBJ	rx_msg;
+	VCI_CAN_OBJ	tx_msg;
+	vic.AccCode = 0x00000000;
+	vic.AccMask = 0xFFFFFFFF;
+	vic.Filter = 0;
+	vic.Mode = 0;
+	vic.Timing0 = 0x03;
+	vic.Timing1 = 0x1C;
+	vic.Reserved = 0;
+
+
+	VCI_Receive(device_type, device_ind, can_ind, &rx_msg, 1, 1);
+
+	CString	a;
+	a.Format(_T("0x%08x"), (int)(rx_msg.ID));
+	CString	b;
+	b.Format(_T("%02x"), (int)(rx_msg.DataLen));
+	CString	c;
+	CString d;
+
+	for (int i = 0; i < rx_msg.DataLen; ++i) {
+
+		d.Format(_T("%02x  "), rx_msg.Data[i]);
+		c += d;
+	}
+
+	SetDlgItemText(IDC_ID, a);
+	SetDlgItemText(IDC_DLC, b);
+	SetDlgItemText(IDC_DATA, c);
+	VCI_ClearBuffer(device_type, device_ind, can_ind);
+	AfxMessageBox(_T("123256456"));
+	//Sleep(10000);*/
+}
+
+
+void Cflash_apiDlg::OnEnChangeId()
+{
+	// TODO:  如果该控件是 RICHEDIT 控件，它将不
+	// 发送此通知，除非重写 CDialogEx::OnInitDialog()
+	// 函数并调用 CRichEditCtrl().SetEventMask()，
+	// 同时将 ENM_CHANGE 标志“或”运算到掩码中。
+
+	// TODO:  在此添加控件通知处理程序代码
+}
+
+
+void Cflash_apiDlg::OnEnChangeDlc()
+{
+	// TODO:  如果该控件是 RICHEDIT 控件，它将不
+	// 发送此通知，除非重写 CDialogEx::OnInitDialog()
+	// 函数并调用 CRichEditCtrl().SetEventMask()，
+	// 同时将 ENM_CHANGE 标志“或”运算到掩码中。
+
+	// TODO:  在此添加控件通知处理程序代码
+}
+
+
+void Cflash_apiDlg::OnEnChangeData()
+{
+	// TODO:  如果该控件是 RICHEDIT 控件，它将不
+	// 发送此通知，除非重写 CDialogEx::OnInitDialog()
+	// 函数并调用 CRichEditCtrl().SetEventMask()，
+	// 同时将 ENM_CHANGE 标志“或”运算到掩码中。
+
+	// TODO:  在此添加控件通知处理程序代码
 }
--- flash_api/flash_apiDlg.h
+++ flash_api/flash_apiDlg.h
@@ -32,13 +32,26 @@ protected:
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
 public:
+
+	CProgressCtrl FlashUpdateProgress;
 	afx_msg void OnBnClickedOk();
-	afx_msg void OnBnClickedCancel();
-	afx_msg void OnBnClickedButton3();
 	afx_msg void OnBnClickedAdd();
 	afx_msg void OnEnChangeLeft();
 	afx_msg void OnEnChangeResult();
-	afx_msg void OnEnChangego();
 	afx_msg void OnEnChangeGoogle();
 	afx_msg void OnBnClickedStart();
+	afx_msg void OnBnClickedClose();
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnBnClickedOpendevice();
+	afx_msg void OnCbnSelchangemoduleselect();
+	afx_msg void OnLbnSelchangeList1();
+	afx_msg void OnNMCustomdrawProgress1(NMHDR *pNMHDR, LRESULT *pResult);
+	afx_msg void OnCbnSelchangeDspSelect();
+	afx_msg void OnEnChangeMfceditbrowse1();
+	afx_msg void OnStnClickedOutTxt();
+	afx_msg void OnEnChangeOutTxt();
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnEnChangeId();
+	afx_msg void OnEnChangeDlc();
+	afx_msg void OnEnChangeData();
 };
